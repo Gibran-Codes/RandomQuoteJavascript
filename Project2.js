1535f360ffef346df2b09fe431fbe607af7ebf81
Gibran Supadi <gibransupadi06@gmail.com>
2024-09-16 21:03:57 +0700

    initial commit

---
+++ Project2.js
@@ -0,0 +1,15 @@
+const accordion = document.getElementsByClassName("content-container");
+
+for (let i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener("click", function () {
+        const isActive = this.classList.contains("active");
+
+        for (let j = 0; j < accordion.length; j++) {
+            accordion[j].classList.remove("active");
+        }
+
+        if (!isActive) {
+            this.classList.add("active");
+        }
+    });
+}
